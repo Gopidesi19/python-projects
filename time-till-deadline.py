cc1ff743da31232ba190ab7631d71793bfdeb684
Gopidesi19 <145692134+Gopidesi19@users.noreply.github.com>
2024-12-18 23:46:54 +0530

    Update time-till-deadline.py

--- time-till-deadline.py
+++ time-till-deadline.py
@@ -1,12 +1,11 @@
 import datetime
 
-user_input = input("enter your goal with deadline\n")
+user_input = input("python:18.12.2025")
 input_list = user_input.split(":")
 goal = input_list[0]
 deadline = input_list[1]
 
 deadline_date = datetime.datetime.strptime(deadline, "%d.%m.%Y")
-#calculate how many days from now till deadline
 today_date = datetime.datetime.today()
 time_till = deadline_date-today_date
 print(f"Dear user time remaining for your learning {goal} is {time_till.days} days")
